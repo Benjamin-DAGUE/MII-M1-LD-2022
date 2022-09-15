2c2fa813537366292f99964aa4cab76b772cb4e5
Benjamin DAGUE <bdague@grindboxlab.fr>
2022-09-15 12:18:12 +0200

    Création de mon premier projet

---
+++ MII/MII.PremierProjet/Contact.cs
@@ -0,0 +1,39 @@
+﻿namespace MII.PremierProjet;
+
+internal class Contact : object
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    #region propfull
+    //private string _FirstName;
+
+    //public string FirstName
+    //{
+    //    get { return _FirstName; }
+    //    set { _FirstName = value; }
+    //}
+
+
+    //private string _LastName;
+    ////La syntaxe suivante utilise la notion de "bodied"
+    //public string LastName
+    //{
+    //    get => _LastName;
+    //    set => _LastName = value?.ToUpper(); 
+    //}
+    #endregion
+
+    #region Constructors
+
+    ////Il existe un constructeur par défaut sans argument.
+    ////Ce constructeur n'est plus créé à partir du moment où un constructeur est défini.
+    //public Contact(string lastName, string firstName = null)
+    //{
+    //    FirstName = firstName;
+    //    //Constructeur
+    //}
+    #endregion
+
+    public override string ToString() => FirstName + " " + LastName;
+}
---
+++ MII/MII.PremierProjet/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MII.PremierProjet;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Contact contact = new Contact()
+        {
+            FirstName = "Benjamin",
+            LastName = "DAGUÉ"
+        };
+
+        Console.WriteLine(contact.ToString());
+
+    }
+}
