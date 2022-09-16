d5de79bde7eeaddf4c1f1694a53af27f6400b23a
Benjamin DAGUE <bdague@grindboxlab.fr>
2022-09-16 17:12:53 +0200

    Mise en place d'un composant pour une carte d'un item

---
+++ PremierProjetBlazorServer/PremierProjetBlazorServer/Pages/ContactCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using PremierProjetBlazorServer.Model;
+
+namespace PremierProjetBlazorServer.Pages;
+
+public partial class ContactCard
+{
+	#region Properties
+
+	[Parameter]
+	public Contact? Contact { get; set; }
+
+	#endregion
+
+}
