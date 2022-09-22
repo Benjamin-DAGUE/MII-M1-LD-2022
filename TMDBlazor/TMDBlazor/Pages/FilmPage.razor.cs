926dabc579f2ca0f9f1dc4fe8b2a9fb5162d27cb
Benjamin DAGUE <bdague@grindboxlab.fr>
2022-09-22 10:11:48 +0200

    Ajout de TMDBlazor : intégration de MudBlazor et de TMDBLib

---
+++ TMDBlazor/TMDBlazor/Pages/FilmPage.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using TMDbLib.Objects.Movies;
+
+namespace TMDBlazor.Pages;
+
+/// <summary>
+///		Page d'un film.
+/// </summary>
+public partial class FilmPage
+{
+	#region Properties
+
+	/// <summary>
+	///		Obtient ou définit l'identifiant du film à afficher.
+	/// </summary>
+	[Parameter]
+	public int Identifier { get; set; }
+
+	/// <summary>
+	///		Obtient ou définit le film.
+	/// </summary>
+	public Movie? Movie { get; set; }
+
+	#endregion
+
+	#region Methods
+
+	/// <inheritdoc/>
+	protected override async Task OnParametersSetAsync()
+	{
+        if (Identifier > 0)
+        {
+			//Lorsque l'identifiant est défini, on appel en async l'API pour récupérer le film correspondant.
+            Movie = await DataClient.GetMovieAsync(Identifier);
+        }
+    }
+
+	#endregion
+}
