6848bf8ea7cb736da292d8c1db752e6541806b0b
Benjamin DAGUE <bdague@grindboxlab.fr>
2022-09-22 09:34:47 +0200

    Finalisation de Blaztermind

--- Blaztermind/Blaztermind/Shared/RedirectTo.razor.cs
+++ Blaztermind/Blaztermind/Shared/RedirectTo.razor.cs
@@ -1,19 +1,24 @@
-﻿namespace Blaztermind.Shared;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blaztermind.Shared;
 
 /// <summary>
 ///     Composant qui redirige l'utilisateur sur la page d'accueil dès qu'il est affiché.
 ///     Peut être utilisée par exemple dans App au niveau du NotFound.
 /// </summary>
-public partial class RedirectToHome
+public partial class RedirectTo
 {
     #region Methods
+
+    [Parameter]
+    public string Route { get; set; } = "/";
 
     /// <inheritdoc/>
     protected override void OnInitialized()
     {
         base.OnInitialized();
 
-        NavMan.NavigateTo("/", true);
+        NavMan.NavigateTo(Route, true);
     }
 
     #endregion
